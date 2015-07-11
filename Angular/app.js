bf47ee95bc2fbfeacba0c2cb949e888bd1b68f47
Phuc Anh (April) Tran <april_tran@brown.edu>
2015-07-11 17:28:38 -0400

    Twitter geodata drawn on map functionality

--- Angular/app.js
+++ Angular/app.js
@@ -12,17 +12,29 @@ hypeMap.service('hypeMapService', function() {
 // CONTROLLERS
 hypeMap.controller('hypeMapController', ['$scope', 'hypeMapService', function($scope) {
 	// MAP BOX ISH
-	mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nZWxoYWNrc3F1YWQiLCJhIjoiZDAwYmMwMTcwMzQ0NTdiMmUzMGJmNWZjNmFmOTI2OGYifQ.ifIhIKtHhbExiHiCXqFoIw';
-	
-	var map = new mapboxgl.Map({
-		container: 'map', // container id
-		style: 'https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v7.json', //stylesheet location
-		center: [40.723, -73.98], // starting position
-		zoom: 12.5 // starting zoom
-	});
+	L.mapbox.accessToken = 'pk.eyJ1IjoiYW5nZWxoYWNrc3F1YWQiLCJhIjoiZDAwYmMwMTcwMzQ0NTdiMmUzMGJmNWZjNmFmOTI2OGYifQ.ifIhIKtHhbExiHiCXqFoIw';
 
-	//Initial Value
-	$scope.data = {};
+	var map = L.mapbox.map('map', 'mapbox.streets').setView([40.723, -73.98], 14);
+
+	//console.log('mapd');
+	/*L.mapbox.featureLayer({
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        // coordinates here are in longitude, latitude order because
+        // x, y is the standard for GeoJSON and many formats
+        coordinates: [-73.98, 40.723]
+    },
+    properties: {
+        title: 'Peregrine Espresso',
+        description: '1718 14th St NW, Washington, DC',
+        // one can customize markers by adding simplestyle properties
+        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+        'marker-size': 'large',
+        'marker-color': '#BE9A6B',
+        'marker-symbol': 'cafe'
+    }
+	}).addTo(map);*/
 
 	var socket = io();
 	socket.on('log', function(msg) {
@@ -31,15 +43,46 @@ hypeMap.controller('hypeMapController', ['$scope', 'hypeMapService', function($s
 
 	//TODO Add marker to map (can call mapTweets function)
 	socket.on('tweet', function(tweet){
-		$('#msgwindow').append('<li class="tweet">' + tweet.user.name + " (@" +
-		tweet.user.screen_name + '): ' + tweet.text + ' | ' +
-		tweet.geo.coordinates[0] + ', ' + tweet.geo.coordinates[1]);
-		console.log(tweet);
+		//$('#msgwindow').append('<li class="tweet">' + tweet.user.name + " (@" +
+		//tweet.user.screen_name + '): ' + tweet.text + ' | ' + tweet.geo.coordinates[0] + ', ' + tweet.geo.coordinates[1]);
+		$scope.mapTweet(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
+		//console.log(tweet);
 	});
 
+	/*window.setTimeout(function() {
+		console.log("asdfasdf");
+        $scope.mapTweet();
+  }, 500);*/
+
 	//Functions
-	$scope.mapTweets = function() {
-		console.log("Map all the tweets!!");
+	$scope.mapTweet = function(lat, lng) {
+		console.log("Mapping...!!!");
+		console.log("lat: " + lat);
+		console.log("lng: " + lng);
+
+		if (!lat || !lng) {
+			return;
+		}
+
+		L.mapbox.featureLayer({
+	    type: 'Feature',
+	    geometry: {
+	        type: 'Point',
+	        // coordinates here are in longitude, latitude order because
+	        // x, y is the standard for GeoJSON and many formats
+	        //coordinates: [-73.98, 40.723]
+	        coordinates: [lng, lat]
+	    },
+	    properties: {
+	        title: 'Peregrine Espresso',
+	        description: '1718 14th St NW, Washington, DC',
+	        // one can customize markers by adding simplestyle properties
+	        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+	        'marker-size': 'large',
+	        'marker-color': '#BE9A6B',
+	        'marker-symbol': 'cafe'
+	    }
+		}).addTo(map);
 	}
 }]);
 
