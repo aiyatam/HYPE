529ed4b5275326e80c19d153fe58c8894a3b5871
Andrew Tam <andrew.tam.sp@gmail.com>
2015-07-11 13:15:07 -0400

    some boiler plate code

---
+++ Angular/app.js
@@ -0,0 +1,13 @@
+var hypeMap = angular.module('hypeMap', []);
+
+// SERVICES
+
+// CONTROLLERS
+hypeMap.controller('hypeMapController', function($scope) {
+	//Initial Value
+	$scope.data = {};
+
+	$scope.mapTweets = function() {
+		console.log("Map all the tweets!!");
+	}
+});
