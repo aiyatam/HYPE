30f5825e231ab209d2ec13f1c929be9ce79d383a
Gabe Fernando <gef756@gmail.com>
2015-07-11 13:58:14 -0400

    ENH: basic server functionality

---
+++ index.js
@@ -0,0 +1,34 @@
+var Twit = require('twit');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var server_port = 3000;
+var server_ip_address = '0.0.0.0';
+
+var T = new Twit({
+  consumer_key: '***REMOVED***',
+  consumer_secret: '***REMOVED***',
+  access_token: '***REMOVED***',
+  access_token_secret: '***REMOVED***'
+});
+
+var twitStream = T.stream('statuses/sample');
+
+function startClientStream() {
+  io.on('connection', function(socket) {
+    socket.emit('log', 'Connection established.');
+  });
+
+  console.log('Stream to clients ready.');
+}
+startClientStream();
+
+twitStream.on('tweet', function (tweet) {
+  io.emit('tweet', tweet);
+});
+
+http.listen(server_port, server_ip_address, function() {
+  console.log('HYPE.map is online.');
+});
