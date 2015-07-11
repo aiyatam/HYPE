b498fca405becd334a5ff472b7867b0c5bb338ae
Phuc Anh (April) Tran <april_tran@brown.edu>
2015-07-11 13:30:45 -0400

    Code style conventions

--- Angular/app.js
+++ Angular/app.js
@@ -1,10 +1,10 @@
-var hypeMap = angular.module('hypeMap', []);
+var hypeMap = angular.module('hypeMap', [ngRoute]);
 
 // SERVICES
 
 // CONTROLLERS
 hypeMap.controller('hypeMapController', function($scope) {
-	//Initial Value
+	// Initial Value
 	$scope.data = {};
 
 	$scope.mapTweets = function() {
