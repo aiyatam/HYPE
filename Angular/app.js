30cf5527db31270484ac7202dd85ea6c2cd78212
Andrew Tam <andrew.tam.sp@gmail.com>
2015-07-11 13:29:48 -0400

    fix this up

--- Angular/app.js
+++ Angular/app.js
@@ -1,12 +1,16 @@
+'use strict'
+
 var hypeMap = angular.module('hypeMap', []);
+console.log("test");
 
 // SERVICES
 
 // CONTROLLERS
 hypeMap.controller('hypeMapController', function($scope) {
+
 	//Initial Value
 	$scope.data = {};
-
+	console.log("blah blah blah");
 	$scope.mapTweets = function() {
 		console.log("Map all the tweets!!");
 	}
