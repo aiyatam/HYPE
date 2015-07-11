d04059b4480503e674716d167a51381573f85398
Andrew Tam <andrew.tam.sp@gmail.com>
2015-07-11 14:02:45 -0400

    move map code into controller

--- Angular/app.js
+++ Angular/app.js
@@ -6,10 +6,22 @@ console.log("test");
 // SERVICES
 hypeMap.service('hypeMapService', function() {
 	console.log("testService");
+	//TODO return dummy data (JSON)
 });
 
 // CONTROLLERS
 hypeMap.controller('hypeMapController', ['$scope', 'hypeMapService', function($scope) {
+
+	function initialize() {
+		var mapCanvas = document.getElementById('map-canvas');
+		var mapOptions = {
+		  center: new google.maps.LatLng(40.7127, -74.0059),
+		  zoom: 8,
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(mapCanvas, mapOptions)
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 	//Initial Value
 	$scope.data = {};
