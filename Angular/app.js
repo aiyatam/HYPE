257f6145dd725eaa8027c498888b19da7d084387
Andrew Tam <andrew.tam.sp@gmail.com>
2015-07-11 17:47:04 -0400

    minor cleanup

--- Angular/app.js
+++ Angular/app.js
@@ -13,48 +13,20 @@ hypeMap.service('hypeMapService', function() {
 hypeMap.controller('hypeMapController', ['$scope', 'hypeMapService', function($scope) {
 	// MAP BOX ISH
 	L.mapbox.accessToken = 'pk.eyJ1IjoiYW5nZWxoYWNrc3F1YWQiLCJhIjoiZDAwYmMwMTcwMzQ0NTdiMmUzMGJmNWZjNmFmOTI2OGYifQ.ifIhIKtHhbExiHiCXqFoIw';
-
 	var map = L.mapbox.map('map', 'mapbox.streets').setView([40.723, -73.98], 14);
-
-	//console.log('mapd');
-	/*L.mapbox.featureLayer({
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        // coordinates here are in longitude, latitude order because
-        // x, y is the standard for GeoJSON and many formats
-        coordinates: [-73.98, 40.723]
-    },
-    properties: {
-        title: 'Peregrine Espresso',
-        description: '1718 14th St NW, Washington, DC',
-        // one can customize markers by adding simplestyle properties
-        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-        'marker-size': 'large',
-        'marker-color': '#BE9A6B',
-        'marker-symbol': 'cafe'
-    }
-	}).addTo(map);*/
 
 	var socket = io();
 	socket.on('log', function(msg) {
 		$('#msgwindow').append('<li class="log">' + msg);
 	});
 
-	//TODO Add marker to map (can call mapTweets function)
 	socket.on('tweet', function(tweet){
-		//$('#msgwindow').append('<li class="tweet">' + tweet.user.name + " (@" +
-		//tweet.user.screen_name + '): ' + tweet.text + ' | ' + tweet.geo.coordinates[0] + ', ' + tweet.geo.coordinates[1]);
-		$scope.mapTweet(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
-		//console.log(tweet);
+		//$('#msgwindow').append('<li class="tweet">' + tweet.user.name + " (@" + tweet.user.screen_name + '): ' + tweet.text + ' | ' + tweet.geo.coordinates[0] + ', ' + tweet.geo.coordinates[1]);
+		if (tweet.geo && tweet.geo.coordinates && tweet.geo.coordinates[0] && tweet.geo.coordinates[1]) {
+			$scope.mapTweet(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
+		}
 	});
 
-	/*window.setTimeout(function() {
-		console.log("asdfasdf");
-        $scope.mapTweet();
-  }, 500);*/
-
-	//Functions
 	$scope.mapTweet = function(lat, lng) {
 		console.log("Mapping...!!!");
 		console.log("lat: " + lat);
