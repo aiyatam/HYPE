ab8569a0afe6d7bad78fc679df0f4b2a5f8e5f5f
Miriam Ashton <miriam_ashton@brown.edu>
2015-07-12 00:04:10 -0400

    parsing tweets

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var server_port = process.env.PORT || 3000;
+var server_port = 3000;
 var server_ip_address = '0.0.0.0';
-// var passport = require('passport');
-// var InstagramStrategy = require('passport-instagram').Strategy;
-// var request = require('request');
+var bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'Angular')));
 
-
-/* TWIT */
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
 var secrets = {
   one : {
@@ -31,14 +31,11 @@ var secrets = {
   }
 }
 var T = new Twit({
-  consumer_key: secrets.two.key,
-  consumer_secret: secrets.two.secret,
-  access_token: secrets.two.token,
-  access_token_secret: secrets.two.token_secret
+  consumer_key: secrets.one.key,
+  consumer_secret: secrets.one.secret,
+  access_token: secrets.one.token,
+  access_token_secret: secrets.one.token_secret
 });
-
-// southwest corner, northeast corner
-var twitStream = T.stream('statuses/filter', { locations : [ '-74.04', '40.7', '-74', '40.88' ] });
 
 function startClientStream() {
   io.on('connection', function(socket) {
@@ -50,32 +47,61 @@ function startClientStream() {
 }
 startClientStream();
 
-twitStream.on('tweet', function (tweet) {
+function startsWith(word, pre) {
+  return word.indexOf(pre) == 0;
+}
+
+function onTweet(tweet) {  
   var text = tweet.text;
-  var text_no_links = text;
-  var link = null;
-  if (text.indexOf("http") != -1) {
-    var i = text.indexOf("http");
-    text_no_links = text.substring(0, i).trim();
-    link = text.substring(i).trim();
+
+  // ignore direct tweets
+  if (startsWith(text, "@")) {
+    return;
   }
 
-  tweet.text_no_links = text_no_links;
-  tweet.link = link;
+  // parse tweets & remove links
+  var wordlist = text.split(" ");
+  var formattedWords = [];
+  var linkList = [];
 
-  io.emit('tweet', tweet);
+  for (var w in wordlist) {
+    var word = wordlist[w];
+    if (word.length > 1 && startsWith(word, "@")) {
+      formattedWords.push("<handle>" + word + "</handle>");
+    }
+    else if (startsWith(word, "#")) {
+      formattedWords.push("<hash>" + word + "</hash>");
+    }
+    else if (startsWith(word, "http")) {
+      linkList.push(word.trim()); // add to link array instead
+    }
+    else {
+      formattedWords.push(word);
+    }
+  }
+
+  tweet.text_no_links = formattedWords.join(" ");
+  tweet.links = linkList;
+  io.emit('tweet', tweet);  
+}
+
+var twitStream = T.stream('statuses/filter', { locations : [ '-74.04', '40.7', '-74', '40.88' ] });
+twitStream.on('tweet', onTweet);
+
+
+// Get user location
+app.post('/coordinates', function(req, res) {
+  var lat = req.body.lat,
+      lon = req.body.lon;
+  if (lat && lon) {
+    twitStream.stop();
+    console.log("Starting stream based on user coordinates");
+    twitStream = T.stream('statuses/filter', {
+      locations : [ parseFloat(lon)-0.1, parseFloat(lat)-0.1, parseFloat(lon)+0.1, parseFloat(lat)+0.1 ]
+    });
+    twitStream.on('tweet', onTweet);
+  }
 });
-
-/* GRAM */
-
-// app.get('/insta', function(req, res) {
-//   request('https://api.instagram.com/v1/media/search?lat=40.7127&lng=74.0059&access_token=607641620.41012ab.297b3487f1e04b82bd440c3666230fae', function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       var info = JSON.parse(body);
-//       res.send(info);
-//     }
-//   });
-// });
 
 http.listen(server_port, server_ip_address, function() {
   console.log('HYPE.map is online.');
