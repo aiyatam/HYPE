7d58ccd7838d02c70563c3b75f7c18524e503648
Andrew Tam <andrew.tam.sp@gmail.com>
2015-08-10 12:05:00 -0400

    finished wrapping up socket.io in a service wrapper

--- Angular/js/services/socket.js
+++ Angular/js/services/socket.js
@@ -7,45 +7,28 @@
 
 		return {
 			on: function(eventName, callback) {
-
+				if (callback) {
+					socket.on(eventName, function() {
+					var args = arguments;
+						$rootScope.$apply(function() {
+							callback.apply(socket, args);
+						});
+					});
+				}
+			},
+			emit: function(eventName, data, callback) {
+				if (callback) {
+					socket.emit(eventName, data, function() {
+						var args = arguments;
+						$rootScope.$apply(function() {
+							callback.apply(socket, args);
+						});
+					});
+				}
 			}
 		};
-
-		socket.on('connect', function() {
-			updateUserCoordinates(map);
-		});
-
-		socket.on('log', function(msg) {
-			$('#msgwindow').append('<li class="log">' + msg);
-		});
-
-		socket.on('tweet', function(tweet) {
-			var isHYPE = tweet.user.followers_count > 5000;
-			if (isHYPE) {
-				console.log("HYPE TWEET FROM " + tweet.user.screen_name);
-			}
-			
-			$scope.mapTweet(tweet.latitude, tweet.longitude, tweet.text_no_links, tweet.user.screen_name, isHYPE);
-
-			var hypeClass = isHYPE ? "hypeTweet" : "";
-			var htmlString = '<a href="https://twitter.com/' + tweet.user.screen_name + '" target="_blank">' + 
-							 '<b class="tweeter">@' + tweet.user.screen_name + '</b></a>: ' + 
-							 tweet.text_no_links;
-			
-			if (tweet.entities.media) {
-				htmlString = '<div class="img-wrap"><img src="' + tweet.entities.media[0].media_url + '" height="80px"></div><div class="descr">' + htmlString + '</div>';
-			}
-
-			$('#messages').append($('<li class="' + hypeClass + '"">').html(htmlString)); // TODO add image from tweet.links
-			$('#chat-scroll').scrollTop($('#chat-scroll')[0].scrollHeight);
-
-			//Rotate Hype Compass
-			$scope.rotation += 10;
-			$('#hypecompass').rotate($scope.rotation);
-		});
 	};
 
 	SocketService.$inject = ['$rootScope'];
 	angular.module('socketService', []).factory('Socket', SocketService);
-
 })();
