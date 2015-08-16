91214d608fe6d066dc48c51e19eecf14aca394cd
Andrew Tam <andrew.tam.sp@gmail.com>
2015-08-16 17:54:36 -0400

    create httpInterceptor service. Useful later for auth tokens and loading animations

--- Angular/js/core.js
+++ Angular/js/core.js
@@ -1,5 +1,10 @@
-//Initialize "hypeMap" Angular Application
 (function() {
 	'use strict';
-	angular.module('HypeApp', ['HypeMap', 'Socket']);
+	
+	angular.module('HypeApp', ['HypeMap', 'Socket', 'Http']);
+
+	angular.module('HypeApp').config(function($httpProvider) {
+		$httpProvider.interceptors.push('HttpInterceptorService');
+	});
+
 })();
